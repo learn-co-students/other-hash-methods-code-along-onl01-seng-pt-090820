b78f71b426ad35fc1e58574222bb10bc7777caa0
S1umish <sunitee.mishra@gmail.com>
2020-09-17 15:38:52 -0400

    Done.

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 #This is the groceries hash we'll be passing in to the get_the_keys method
 # groceries = {
@@ -8,6 +8,18 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
-def get_the_keys(groceries)
-  #code your solution here!
+
+def get_the_keys
+  groceries = 
+  {
+    dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
+  }
 end
+
+def get_the_keys(groceries)
+  groceries.keys
+end
+
